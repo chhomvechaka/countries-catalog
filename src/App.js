b4364b002977e5fb3102a85636e20528cf8b13e1
chhomvechaka <chhomvechaka@gmail.com>
2024-08-05 08:18:26 +0700

    modified route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<CountryListDisplayLayout />} />
+                <Route path="/countries-catalog" element={<CountryListDisplayLayout />} />
             </Routes>
         </BrowserRouter>
     );
